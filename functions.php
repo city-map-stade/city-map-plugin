2e541675f82185009dc5ccb974baed9eee35dc4f
Maurice Seir <m.seir@city-map.de>
2020-11-24 10:10:26 +0100

    changed some commits in functions.php

--- functions.php
+++ functions.php
@@ -8,74 +8,80 @@
  * Author URI: https://internet-erfolg.de
  */
 
+// Plugins die wir installieren, für Seiten von Kunden
+// Name ist immer der Name des Plugins
+// Slug ist der Ordnername, der nach dem entpacken der Datei gezeigt wird
+// Source ist die Quelle des Plugins, i.d.R. liegt dies immer auf wordpress.internet-erfolg.de --> _plugins
+// required ist ein Boolean-Wert, ob das Plugin installiert werden muss oder nur empfohlen wird, Standardmäßig true eingestelt
+
 require_once dirname( __FILE__ ) . '/class-tgm-plugin-activation.php';
 
 add_action( 'tgmpa_register', 'city_map_register_required_plugins' );
 
 function city_map_register_required_plugins() {
 	$plugins = array(
-		// This is an example of how to include a plugin bundled with a theme.
+
 		array(
-			'name'               => 'All 404 Redirect to Homepage', // The plugin name.
-			'slug'               => 'all-404-redirect-to-homepage', // The plugin slug (typically the folder name).
-			'source'             => 'https://wordpress.internet-erfolg.de/_plugins/404-redirect.zip', // The plugin source.
-			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+			'name'               => 'All 404 Redirect to Homepage',
+			'slug'               => 'all-404-redirect-to-homepage',
+			'source'             => 'https://wordpress.internet-erfolg.de/_plugins/404-redirect.zip',
+			'required'           => true,
 		),
 		
 		array(
-			'name'               => 'Attachment Pages Redirect', // The plugin name.
-			'slug'               => 'attachment-pages-redirect', // The plugin slug (typically the folder name).
-			'source'             => 'https://wordpress.internet-erfolg.de/_plugins/pages-redirect.zip', // The plugin source.
-			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+			'name'               => 'Attachment Pages Redirect',
+			'slug'               => 'attachment-pages-redirect',
+			'source'             => 'https://wordpress.internet-erfolg.de/_plugins/pages-redirect.zip',
+			'required'           => true,
 		),
 		
 		array(
-			'name'               => 'Custom CSS & JS', // The plugin name.
-			'slug'               => 'custom-css-js', // The plugin slug (typically the folder name).
-			'source'             => 'https://wordpress.internet-erfolg.de/_plugins/custom-css.zip', // The plugin source.
-			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+			'name'               => 'Custom CSS & JS',
+			'slug'               => 'custom-css-js',
+			'source'             => 'https://wordpress.internet-erfolg.de/_plugins/custom-css.zip',
+			'required'           => true,
 		),
 		
 		array(
-			'name'               => 'Disable Gutenberg', // The plugin name.
-			'slug'               => 'disable-gutenberg', // The plugin slug (typically the folder name).
-			'source'             => 'http://wordpress.internet-erfolg.de/_plugins/disable-gutenberg.zip', // The plugin source.
-			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+			'name'               => 'Disable Gutenberg',
+			'slug'               => 'disable-gutenberg',
+			'source'             => 'http://wordpress.internet-erfolg.de/_plugins/disable-gutenberg.zip',
+			'required'           => true,
 		),
 		
 		array(
-			'name'               => 'Elementor Pro', // The plugin name.
-			'slug'               => 'elementor-pro', // The plugin slug (typically the folder name).
-			'source'             => 'https://wordpress.internet-erfolg.de/_plugins/elementor-pro.zip', // The plugin source.
-			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+			'name'               => 'Elementor Pro',
+			'slug'               => 'elementor-pro',
+			'source'             => 'https://wordpress.internet-erfolg.de/_plugins/elementor-pro.zip',
+			'required'           => true,
 		),
 		
 		array(
-			'name'               => 'eRecht24', // The plugin name.
-			'slug'               => 'erecht24', // The plugin slug (typically the folder name).
-			'source'             => 'https://wordpress.internet-erfolg.de/_plugins/erecht24.zip', // The plugin source.
-			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+			'name'               => 'eRecht24',
+			'slug'               => 'erecht24',
+			'source'             => 'https://wordpress.internet-erfolg.de/_plugins/erecht24.zip',
+			'required'           => true,
 		),
 		
 		array(
-			'name'               => 'HT Mega', // The plugin name.
-			'slug'               => 'ht-mega-for-elementor', // The plugin slug (typically the folder name).
-			'source'             => 'https://wordpress.internet-erfolg.de/_plugins/ht-mega-for-elementor.zip', // The plugin source.
-			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+			'name'               => 'HT Mega',
+			'slug'               => 'ht-mega-for-elementor',
+			'source'             => 'https://wordpress.internet-erfolg.de/_plugins/ht-mega-for-elementor.zip',
+			'required'           => true,
 		),
 		
 		array(
-			'name'               => 'Wordfence', // The plugin name.
-			'slug'               => 'wordfence', // The plugin slug (typically the folder name).
-			'source'             => 'https://wordpress.internet-erfolg.de/_plugins/wordfence.zip', // The plugin source.
-			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+			'name'               => 'Wordfence',
+			'slug'               => 'wordfence',
+			'source'             => 'https://wordpress.internet-erfolg.de/_plugins/wordfence.zip',
+			'required'           => true,
 		),
 		
 		array(
-			'name'               => 'WP Mail SMTP', // The plugin name.
-			'slug'               => 'wp-mail-smtp.2.4.0', // The plugin slug (typically the folder name).
-			'source'             => 'https://wordpress.internet-erfolg.de/_plugins/wp-mail-smtp.zip', // The plugin source.
-			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+			'name'               => 'WP Mail SMTP',
+			'slug'               => 'wp-mail-smtp.2.4.0',
+			'source'             => 'https://wordpress.internet-erfolg.de/_plugins/wp-mail-smtp.zip',
+			'required'           => true,
 		)
 	);
 	
